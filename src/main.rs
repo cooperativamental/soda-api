2b4be4b836424bcfa21c5dfb9302443a39751636
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2023-07-13 17:11:03 -0300

    dinamic

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 mod default_template;
-use rocket::serde::{Deserialize, json::{Json, serde_json::json, Value}};
+use rocket::serde::{
+    json::{serde_json::json, Json, Value},
+    Deserialize,
+};
 use soda_sol::*;
-use default_template::default_template;
+use std::path;
+use std::path::PathBuf;
+
+const TEMPLATES: &'static [&'static str] = &[
+    "default",
+    "react_native_experimental",
+    "seahorse_experimental",
+];
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Value {
+   // read all the files in the template folder
+    let mut templates = Vec::new();
+    for template in TEMPLATES {
+        let template = load_template(
+            &path::Path::new(&format!("templates/{}.soda", template))
+                .to_string_lossy(),
+        )
+        .unwrap();
+        templates.push(template.metadata);
+    }
+    json!({ "templates": templates.iter().enumerate().collect::<Vec<_>>() })
 }
 
 #[shuttle_runtime::main]
-async fn rocket() -> shuttle_rocket::ShuttleRocket {
+async fn rocket(
+    #[shuttle_static_folder::StaticFolder(folder = "templates")] _static_folder: PathBuf,
+) -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build().mount("/", routes![index, new]);
 
     Ok(rocket.into())
@@ -23,12 +46,22 @@ struct GenerateReq {
     idl: IDL,
 }
 
-#[post("/<template_id>", format = "json", data = "<generate_req>")]
-fn new(template_id: &str, generate_req: Json<GenerateReq>)-> Value  {
-    let GenerateReq { idl } = generate_req.into_inner();
-    println!("idl: {:?}", idl);
-    println!("template_id: {:?}", template_id);
-    let template = default_template();
-    let files = generate_project(template, &idl);
-    json!({ "files": files })
- }
+#[post(
+    "/get_project_files/<template_id>",
+    format = "json",
+    data = "<generate_req>"
+)]
+fn new(template_id: usize, generate_req: Json<GenerateReq>) -> Value {
+    if template_id >= TEMPLATES.len() {
+        return json!({ "error": "Invalid template id" });
+    } else {
+        let GenerateReq { idl } = generate_req.into_inner();
+        let template = load_template(
+            &path::Path::new(&format!("templates/{}.soda", TEMPLATES[template_id]))
+                .to_string_lossy(),
+        )
+        .unwrap();
+        let files = generate_project(template, &idl);
+        json!({ "files": files })
+    }
+}
