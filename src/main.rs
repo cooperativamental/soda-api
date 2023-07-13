d640b98cb6c67b06c8b3d704948535a43fd3d758
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2023-07-13 19:26:51 -0300

    fix git push

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,30 @@
 #[macro_use]
 extern crate rocket;
-
+mod default_template;
 use rocket::serde::{
     json::{serde_json::json, Json, Value},
-    Deserialize,
+    Deserialize
 };
 use soda_sol::*;
-use std::path;
 
-const TEMPLATES: &'static [&'static str] = &[
-    "default",
-    "react_native_experimental",
-    "seahorse_experimental",
-];
-
-#[get("/templates")]
-fn index() -> Value {
-    let mut templates = Vec::new();
-    for template in TEMPLATES {
-        let template = load_template(
-            &path::Path::new(&format!("assets/{}.soda", template))
-                .to_string_lossy(),
-        )
-        .unwrap();
-        templates.push(template.metadata);
-    }
-    json!({ "templates": templates })
-}
 
 #[shuttle_runtime::main]
-async fn rocket(
-) -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index, new]);
-
+async fn rocket() -> shuttle_rocket::ShuttleRocket {
+    let rocket = rocket::build().mount("/", routes![index, templates, get_project_files]);
     Ok(rocket.into())
+}
+
+#[get("/templates")]
+fn templates() -> Value {
+    json!({ "templates":[ default_template::default_template().metadata]  })
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    r#" USAGE:
+        GET https://soda.shuttleapp.rs/templates returns a list of templates
+        POST https://soda.shuttleapp.rs/get_project_files/<template_id> generates a project from a template, been the template_id the index of the template in the list returned by the GET request
+    "#
 }
 
 #[derive(Deserialize)]
@@ -47,17 +38,9 @@ struct GenerateReq {
     format = "json",
     data = "<generate_req>"
 )]
-fn new(template_id: usize, generate_req: Json<GenerateReq>) -> Value {
-    if template_id >= TEMPLATES.len() {
-        return json!({ "error": "Invalid template id" });
-    } else {
-        let GenerateReq { idl } = generate_req.into_inner();
-        let template = load_template(
-            &path::Path::new(&format!("assets/{}.soda", TEMPLATES[template_id]))
-                .to_string_lossy(),
-        )
-        .unwrap();
-        let files = generate_project(template, &idl);
-        json!({ "files": files })
-    }
+fn get_project_files(template_id: usize, generate_req: Json<GenerateReq>) -> Value {
+    let GenerateReq { idl } = generate_req.into_inner();
+    let template = default_template::default_template();
+    let files = generate_project(template, &idl);
+    json!({ "files": files })
 }
